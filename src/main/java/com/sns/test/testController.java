bf0e2aa087d701d70d649896e7950b3af020843f
DESKTOP-I7UUISV\user <wishchaser@naver.com>
2022-10-17 21:42:52 +0900

    sns 초기설정

---
+++ src/main/java/com/sns/test/testController.java
@@ -0,0 +1,31 @@
+package com.sns.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sns.test.dao.TestDAO;
+
+@Controller
+public class testController {
+	
+	@Autowired
+	private TestDAO testDAO;
+	
+	@RequestMapping("/test")
+	public String test() {
+		return "/test";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/test/comment")
+	public Map<String, Object> selectComment(){
+		Map<String, Object> comment = new HashMap<>();
+		comment = testDAO.selectComment();
+		return comment ;
+	}
+}
